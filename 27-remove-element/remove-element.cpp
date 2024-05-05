c639b252f2d8676eb43a0f9d723da63522f2a4dd
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-05-05 19:35:38 +0500

    Time: 5 ms (27.72%) | Memory: 10.4 MB (37.48%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(val == nums[i])
+            {
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
