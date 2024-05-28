62d6d7afffb7953bc49274fc6df49f3f4e9ff4c8
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-05-28 22:10:25 +0500

    Time: 0 ms (100.00%) | Memory: 10.9 MB (28.57%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         int max = 0,n=candies.size();
-        for(int i=0; i<n; i++)
+        vector<int> temp = candies;
+        sort(temp.begin(),temp.end());
+        max = temp[n-1];
+        /*for(int i=0; i<n; i++)
         {
             if(candies[i] >max)
             {
                 max = candies[i];
             }
-        }
+        }*/
         vector<bool> res;
         for(int i=0; i<n; i++)
         {
