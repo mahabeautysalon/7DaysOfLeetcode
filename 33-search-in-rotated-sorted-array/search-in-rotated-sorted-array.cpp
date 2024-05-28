c0d191686487dde1dbdb462e43a2e68bbe23216e
Cyberspace_ <101250636+mahabeautysalon@users.noreply.github.com>
2024-05-28 21:53:40 +0500

    Time: 2 ms (64.61%) | Memory: 13.6 MB (35.38%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int beg=0,end=nums.size()-1,loc=-1;
-        ///vector<int>lower,upper;
         while(beg<=end)
         {
             if(nums[beg] == target)
             {
-                loc = beg;
-                break;
+                return beg;
             }else if(nums[end]==target)
             {
-                loc = end;
-                break;
+                return end;
             }
             beg++;end--;
         }
